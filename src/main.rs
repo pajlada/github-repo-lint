780c183b4ba4f33f1dd127acbb8d5411e5a41560
pajlada <rasmus.karlsson@pajlada.com>
2022-10-06 23:46:29 +0200

    Migrate to clap v4 (#158)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use ::reqwest::blocking::Client;
-use clap::{Arg, Command};
+use clap::{Arg, ArgAction, Command};
 use console::Term;
+use reqwest::blocking::Client;
 
 use const_format::formatcp;
 use log::*;
@@ -33,45 +33,43 @@ fn main() -> Result<(), anyhow::Error> {
                 .long("config")
                 .value_name("FILE")
                 .help("Path to config file to use")
-                .default_value("config.json")
-                .takes_value(true),
+                .default_value("config.json"),
         )
         .arg(
-            Arg::new("v")
+            Arg::new("verbose")
                 .short('v')
-                .multiple_occurrences(true)
+                .long("verbose")
+                .action(ArgAction::Count)
                 .help("Sets the level of verbosity"),
         )
         .arg(
             Arg::new("fix")
                 .long("fix")
+                .action(ArgAction::SetTrue)
                 .help("Try to fix the issues found"),
         )
         .arg(
             Arg::new("repo")
                 .long("repo")
-                .multiple_occurrences(true)
-                .help("Target GitHub repository")
-                .takes_value(true),
+                .action(ArgAction::Append)
+                .help("Target GitHub repository"),
         )
         .arg(
             Arg::new("user")
                 .long("user")
-                .multiple_occurrences(true)
-                .help("Target GitHub user")
-                .takes_value(true),
+                .action(ArgAction::Append)
+                .help("Target GitHub user"),
         )
         .arg(
             Arg::new("organization")
                 .long("organization")
                 .alias("org")
-                .multiple_occurrences(true)
-                .help("Target GitHub organization")
-                .takes_value(true),
+                .action(ArgAction::Append)
+                .help("Target GitHub organization"),
         )
         .get_matches();
 
-    let log_level = match matches.occurrences_of("v") {
+    let log_level = match matches.get_count("verbose") {
         0 => log::LevelFilter::Info,
         1 => log::LevelFilter::Debug,
         _ => log::LevelFilter::Trace,
@@ -83,21 +81,31 @@ fn main() -> Result<(), anyhow::Error> {
         .filter_module(module_path!(), log_level)
         .init();
 
-    let repos: Vec<&str> = matches.values_of("repo").unwrap_or_default().collect();
-    let users: Vec<&str> = matches.values_of("user").unwrap_or_default().collect();
-    let organizations: Vec<&str> = matches
-        .values_of("organization")
+    let repos: Vec<&str> = matches
+        .get_many::<String>("repo")
         .unwrap_or_default()
+        .map(|s| s.as_str())
+        .collect();
+    let users: Vec<&str> = matches
+        .get_many::<String>("user")
+        .unwrap_or_default()
+        .map(|s| s.as_str())
+        .collect();
+    let organizations: Vec<&str> = matches
+        .get_many::<String>("organization")
+        .unwrap_or_default()
+        .map(|s| s.as_str())
         .collect();
 
-    let config_path = Path::new(matches.value_of("config").expect("value MUST be set"));
+    let config_path = Path::new(matches.get_one::<String>("config").unwrap());
 
     let config = config::load_config(config_path)?;
 
-    let github_api_token: String = match std::env::var("GITHUB_API_TOKEN") {
-        Ok(s) => s,
-        Err(_) => return Err(anyhow::anyhow!("xD")),
-    };
+    let github_api_token = std::env::var("GITHUB_API_TOKEN").map_err(|_| {
+        anyhow::anyhow!(
+            "Missing GitHub token, must be defined with the GITHUB_API_TOKEN environment variable."
+        )
+    })?;
 
     info!("Github API root: {:?}", config.github_api_root);
 
@@ -115,8 +123,8 @@ fn main() -> Result<(), anyhow::Error> {
     let api_client = api::new(client, config.github_api_root.as_str())?;
 
     let options = options::Options {
-        dry_run: !matches.is_present("fix"),
-        dry_run_bpr: !matches.is_present("fix"),
+        dry_run: !matches.get_flag("fix"),
+        dry_run_bpr: !matches.get_flag("fix"),
     };
 
     let ctx = context::Context {
